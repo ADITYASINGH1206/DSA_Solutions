b755249c830d73fc13c506eb40f38eded477c853
Aditya Kumar Singh <simghaditya21@gmail.com>
2026-02-13 02:31:22 +0530

    1283. Find the Smallest Divisor Given a Threshold

---
+++ Solutions/1200-1299/1283. Find the Smallest Divisor Given a Threshold/Solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int maxi = INT_MIN;
+        int n = nums.size();
+        for(int i =0;i<n;i++){
+            maxi = max(maxi,nums[i]);
+        }
+        int low = 1;
+        int high = maxi;
+        while(low<=high){
+            int mid= (low+high)/2;
+            int sum = 0;
+            for(int i=0;i<n;i++){
+                sum += ceil((double)nums[i]/(double)mid);
+            }
+            if(sum <= threshold){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+};
