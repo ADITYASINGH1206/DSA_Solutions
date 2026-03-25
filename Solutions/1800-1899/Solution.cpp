4d214a8e8743ae1d0d4742a82d159be18f0ce99d
Aditya Kumar Singh <simghaditya21@gmail.com>
2026-03-26 00:26:36 +0530

    1844. Replace All Digits with Characters

---
+++ Solutions/1800-1899/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string replaceDigits(string s) {
+        int n = s.size();
+        string ans = "";
+        for(int i =0;i<n;i++){
+            if(i%2==0) ans += s[i];
+            else{
+                s[i] = s[i-1] + (s[i]-'0');
+                ans += s[i];
+            }
+        }
+        return ans;
+    }
+};
