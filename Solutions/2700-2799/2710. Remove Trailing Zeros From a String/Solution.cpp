00da0d74cf6eb6d100b67445e0b2ade5b4c18a2a
Aditya Kumar Singh <simghaditya21@gmail.com>
2026-04-09 00:52:50 +0530

    2710. Remove Trailing Zeros From a String

---
+++ Solutions/2700-2799/2710. Remove Trailing Zeros From a String/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeTrailingZeros(string num) {
+        int n = num.size();
+        int i = n-1;
+        while(num[i] == '0'){
+            num.pop_back();
+            i--;
+
+        }
+        return num;
+    }
+};
