3ec23abd7d87f2233f82ed6116bfb7fd93b7d3ef
Aditya Kumar Singh <simghaditya21@gmail.com>
2026-04-10 00:46:43 +0530

    49. Group Anagrams

---
+++ Solutions/0000-0099/49. Group Anagrams/Solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (auto& s : strs) {
+            string k = s;
+            sort(k.begin(), k.end());
+            mp[k].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for (auto& [key, value] : mp){
+            ans.push_back(value);
+        }
+        
+        return ans;
+    }
+};
