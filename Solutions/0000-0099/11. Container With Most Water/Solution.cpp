393c9b8638aecd6fd8e50c2e002aa17ef860ba5b
Aditya Kumar Singh <simghaditya21@gmail.com>
2026-04-11 01:35:38 +0530

    11. Container With Most Water

---
+++ Solutions/0000-0099/11. Container With Most Water/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans = 0;
+        int l = 0;
+        int r = height.size() - 1;
+        
+        while (l < r) {
+            int h = min(height[l], height[r]);
+            int w = r-l; 
+            int area = h*w;
+            ans = max(ans, area);
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return ans;
+    }
+};
